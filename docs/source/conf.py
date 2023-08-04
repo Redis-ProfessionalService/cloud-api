e51520d36178d15311279da80c5f35362ee60474
Khashayar <me@khashayar.me>
2023-08-04 11:49:58 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Redis Cloud REST API Documentation'
+copyright = '2023, Redis Enterprise Professional Services'
+author = 'Khashayar Hajian'
 
 release = '0.1'
 version = '0.1.0'
 
+# -- General configuration ---------------------------------------------------
 # -- General configuration
 
 extensions = [
@@ -28,7 +44,6 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
